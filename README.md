9453c222696420361e265f2bdd7330edcd98dd85
Ted de Gouw <ted@hastur.nl>
2025-02-03 16:31:18 +0100

    Added parameters to the script.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # DicomDumper
 Simple script to dump the video out of a dicom file
+
+## Usage
+
+Install requirements before usage
+```
+python -m pip install -r requirements.txt
+```
+
+And run the script either on a single file or a list of files.
+
+For help on parameters use -h
+```
+python dicomdumper.py -h
+```
+
+To run against a directory use the --directory
+```
+python dicomdumper.py --directory '/path/to/dcmfiles'
+```
+
+To run against a single file use the --file parameter
+```
+python dicomdumper.py --file '/path/to/file.dcm'
+```
--- dicomdumper.py
+++ dicomdumper.py
@@ -1,23 +1,43 @@
+import glob
 import os
 import argparse
 from pydicom import dcmread
 from pydicom.encaps import generate_pixel_data_frame
 
-parser = argparse.ArgumentParser(description="Dump de video vanuit een DCM file.")
-parser.add_argument('input_file', type=str, help="Relatief pad naar de dicom file")
 
+def process_file(filepath: str, extension:str):
+    dataset = None
+    try:
+        dataset = dcmread(filepath)
+    except Exception as err:
+        print(f"Unexpected {err=}")
+        return
+
+    print(f"Patient's Name: {dataset.PatientName}")
+    print(f"Modality: {dataset.Modality}")
+    print(f"Study Date: {dataset.StudyDate}")
+    print(f"Transfer syntax uid: {dataset.file_meta.TransferSyntaxUID}")
+    print(f"Transfer syntax: {dataset.file_meta.TransferSyntaxUID.name}")
+
+    # For now just assume mp4 for now so some kind of video player like VLC opens
+    # and will read the header for the file.
+    output_filename = os.path.splitext(filepath)[0] + extension
+    with open(output_filename, 'wb') as f:
+        f.write(next(generate_pixel_data_frame(dataset.PixelData)))
+
+
+parser = argparse.ArgumentParser(description="Dicom extractor")
+parser.add_argument('--file', type=str, help="Path to single dicom file")
+parser.add_argument('--directory', type=str, help="Path to a directory with multiple dicom files")
+parser.add_argument('--pattern', type=str, default='*.dcm', help="Pattern to match when searching files (default *.dcm)")
+parser.add_argument('--extension', type=str, default='.mp4', help="Extension to set for the extracted file (default .mp4)")
 args = parser.parse_args()
-dicom_file_absolute = os.path.abspath(args.input_file)
-dataset = dcmread(dicom_file_absolute)
+if args.file:
+    print(f'Processing file: {args.file}')
+    process_file(os.path.abspath(args.file), args.extension)
 
-print(f"Patient's Name: {dataset.PatientName}")
-print(f"Modality: {dataset.Modality}")
-print(f"Study Date: {dataset.StudyDate}")
-print(f"Transfer syntax uid: {dataset.file_meta.TransferSyntaxUID}")
-print(f"Transfer syntax: {dataset.file_meta.TransferSyntaxUID.name}")
-
-# Just assume mp4 for now so some kind of video player like VLC opens
-# and will read the header for the file
-output_filename = os.path.splitext(dicom_file_absolute)[0] + ".mp4"
-with open(output_filename , 'wb') as f:
-    f.write(next(generate_pixel_data_frame(dataset.PixelData)))
+if args.directory:
+    dcm_files = glob.glob(os.path.join(args.directory, args.pattern))
+    for dcm_file in dcm_files:
+        print(f'Processing file: {dcm_file}')
+        process_file(os.path.abspath(dcm_file), args.extension)
